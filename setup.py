789088d32407316039dba2041318d0db08cea5e8
David Fiel <github@dfiel.com>
2017-12-20 16:11:53 -0500

    Add dependencies to setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,10 @@ setup(
     name='greenwavecontrol',
     version='0.1',
     packages=[''],
+    install_requires=[
+          'requests',
+          'xmltodict',
+      ],
     url='https://github.com/dfiel/greenwavecontrol',
     license='MIT',
     author='David Fiel',
