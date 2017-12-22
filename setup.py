9fe7281b30664cd4ed0027ec2ea0bdea2a560a97
David Fiel <github@dfiel.com>
2017-12-22 16:10:43 -0500

    Change version to 0.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='greenwavereality',
-    version='0.2',
+    version='0.2.1',
     packages=['greenwavereality'],
     install_requires=[
           'requests',
