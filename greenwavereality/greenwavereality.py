734613a2f2363c54f1e91f98c2fac55c02c0d5ba
David Fiel <github@dfiel.com>
2017-12-23 12:25:45 -0500

    Increment Version

--- greenwavereality/greenwavereality.py
+++ greenwavereality/greenwavereality.py
@@ -2,74 +2,87 @@ import requests
 import xmltodict
 import urllib3
 
+
 def grab_xml(host, token=None):
-    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+    urllib3.disable_warnings()
     if token:
         scheme = "https"
     if not token:
         scheme = "http"
         token = "1234567890"
-    url = (scheme + '://' + host + '/gwr/gop.php?cmd=GWRBatch&data=<gwrcmds><gwrcmd><gcmd>RoomGetCarousel</gcmd><gdata><gip><version>1</version><token>' + token + '</token><fields>name,status</fields></gip></gdata></gwrcmd></gwrcmds>&fmt=xml')
+    url = (
+            scheme + '://' + host + '/gwr/gop.php?cmd=GWRBatch&data=<gwrcmds><gwrcmd><gcmd>RoomGetCarousel</gcmd><gdata><gip><version>1</version><token>' + token + '</token><fields>name,status</fields></gip></gdata></gwrcmd></gwrcmds>&fmt=xml')
     response = requests.get(url, verify=False)
     dict = xmltodict.parse(response.content)
     dict = dict['gwrcmds']['gwrcmd']['gdata']['gip']['room']['device']
     return dict
-  
+
+
 def set_brightness(host, did, value, token=None):
-    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+    urllib3.disable_warnings()
     if token:
         scheme = "https"
     if not token:
         scheme = "http"
         token = "1234567890"
-    url = (scheme + '://' + host + '/gwr/gop.php?cmd=DeviceSendCommand&data=<gip><version>1</version><token>' + token + '</token><did>' + did + '</did><value>' + str(value) + '</value><type>level</type></gip>&fmt=xml')
+    url = (
+            scheme + '://' + host + '/gwr/gop.php?cmd=DeviceSendCommand&data=<gip><version>1</version><token>' + token + '</token><did>' + did + '</did><value>' + str(
+        value) + '</value><type>level</type></gip>&fmt=xml')
     response = requests.get(url, verify=False)
     if response.status_code == '200':
         return True
     else:
         return False
 
+
 def hass_brightness(device):
     if 'level' in device:
-        level = int((int(device['level'])/100)*255)
+        level = int((int(device['level']) / 100) * 255)
         return level
     else:
         return 0
 
+
 def turn_on(host, did, token=None):
-    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+    urllib3.disable_warnings()
     if token:
         scheme = "https"
     if not token:
         scheme = "http"
         token = "1234567890"
-    url = (scheme + '://' + host + '/gwr/gop.php?cmd=DeviceSendCommand&data=<gip><version>1</version><token>' + token + '</token><did>' + did + '</did><value>1</value></gip>&fmt=xml')
+    url = (
+            scheme + '://' + host + '/gwr/gop.php?cmd=DeviceSendCommand&data=<gip><version>1</version><token>' + token + '</token><did>' + did + '</did><value>1</value></gip>&fmt=xml')
     response = requests.get(url, verify=False)
     if response.status_code == '200':
         return True
     else:
         return False
 
+
 def turn_off(host, did, token=None):
-    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+    urllib3.disable_warnings()
     if token:
         scheme = "https"
     if not token:
         scheme = "http"
         token = "1234567890"
-    url = (scheme + '://' + host + '/gwr/gop.php?cmd=DeviceSendCommand&data=<gip><version>1</version><token>' + token + '</token><did>' + did + '</did><value>0</value></gip>&fmt=xml')
+    url = (
+            scheme + '://' + host + '/gwr/gop.php?cmd=DeviceSendCommand&data=<gip><version>1</version><token>' + token + '</token><did>' + did + '</did><value>0</value></gip>&fmt=xml')
     response = requests.get(url, verify=False)
     if response.status_code == '200':
         return True
     else:
         return False
+
 
 def check_online(device):
     return 'offline' not in device
 
+
 def grab_token(host, email, password):
-    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-    url = ('https://' + host + '/gwr/gop.php?cmd=GWRLogin&data=<gip><version>1</version><email>' + str(email) + '</email><password>' + str(password) + '</password></gip>&fmt=xml')
+    urllib3.disable_warnings()
+    url = ('https://' + host + '/gwr/gop.php?cmd=GWRLogin&data=<gip><version>1</version><email>' + str(
+        email) + '</email><password>' + str(password) + '</password></gip>&fmt=xml')
     response = requests.get(url, verify=False)
     dict = xmltodict.parse(response.content)
     dict = dict['gip']['token']
