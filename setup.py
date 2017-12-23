36d5dd310258e81f34eadd54adec4e784fc27849
David Fiel <github@dfiel.com>
2017-12-22 22:17:14 -0500

    Increment version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='greenwavereality',
-    version='0.2.1',
+    version='0.2.2',
     packages=['greenwavereality'],
     install_requires=[
           'requests',
