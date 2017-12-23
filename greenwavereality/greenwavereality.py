cac523f4f94be49dd171d813108a21526ff4d793
David Fiel <github@dfiel.com>
2017-12-22 22:17:00 -0500

    Disable SSL Verification Gateway v3 uses insecure SSL cert

--- greenwavereality/greenwavereality.py
+++ greenwavereality/greenwavereality.py
@@ -8,7 +8,7 @@ def grab_xml(host, token=None):
         scheme = "http"
         token = "1234567890"
     url = (scheme + '://' + host + '/gwr/gop.php?cmd=GWRBatch&data=<gwrcmds><gwrcmd><gcmd>RoomGetCarousel</gcmd><gdata><gip><version>1</version><token>' + token + '</token><fields>name,status</fields></gip></gdata></gwrcmd></gwrcmds>&fmt=xml')
-    response = requests.get(url)
+    response = requests.get(url, verify=False)
     dict = xmltodict.parse(response.content)
     dict = dict['gwrcmds']['gwrcmd']['gdata']['gip']['room']['device']
     return dict
@@ -20,7 +20,7 @@ def set_brightness(host, did, value, token=None):
         scheme = "http"
         token = "1234567890"
     url = (scheme + '://' + host + '/gwr/gop.php?cmd=DeviceSendCommand&data=<gip><version>1</version><token>' + token + '</token><did>' + did + '</did><value>' + str(value) + '</value><type>level</type></gip>&fmt=xml')
-    response = requests.get(url)
+    response = requests.get(url, verify=False)
     if response.status_code == '200':
         return True
     else:
@@ -40,7 +40,7 @@ def turn_on(host, did, token=None):
         scheme = "http"
         token = "1234567890"
     url = (scheme + '://' + host + '/gwr/gop.php?cmd=DeviceSendCommand&data=<gip><version>1</version><token>' + token + '</token><did>' + did + '</did><value>1</value></gip>&fmt=xml')
-    response = requests.get(url)
+    response = requests.get(url, verify=False)
     if response.status_code == '200':
         return True
     else:
@@ -53,7 +53,7 @@ def turn_off(host, did, token=None):
         scheme = "http"
         token = "1234567890"
     url = (scheme + '://' + host + '/gwr/gop.php?cmd=DeviceSendCommand&data=<gip><version>1</version><token>' + token + '</token><did>' + did + '</did><value>0</value></gip>&fmt=xml')
-    response = requests.get(url)
+    response = requests.get(url, verify=False)
     if response.status_code == '200':
         return True
     else:
@@ -64,5 +64,5 @@ def check_online(device):
 
 def grab_token(host, email, password):
     url = ('https://' + host + '/gwr/gop.php?cmd=GWRLogin&data=<gip><version>1</version><email>' + str(email) + '</email><password>' + str(password) + '</password></gip>&fmt=xml')
-    response = requests.get(url)
+    response = requests.get(url, verify=False)
     return response
